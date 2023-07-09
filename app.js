d8a4b90c583e0280f3bcb8f5b5389975e3b81471
EmiTorres93 <emilianat1992@gmail.com>
2023-07-09 09:56:29 -0300

    Add modal

--- app.js
+++ app.js
@@ -4,6 +4,67 @@ const productos = JSON.parse(localStorage.getItem("productos")) || [];
 
 const cuerpoTabla = document.getElementById("cuerpoTabla");
 
+//Va a tener todos los métodos de los modales que vienen de Bootstrap, con esta variable (que tiene todos los métodos) ya podemos abrir el modal
+const myModal = new bootstrap.Modal(document.getElementById("updateModal"));
+
+//Función para Abrir Modal
+const abrirModal = (index) => {
+  document.querySelector(".modal-body").innerHTML = " ";
+  const formularioUpDate = document.createElement("form");
+
+  const contenidoFormulario =
+    /* HTML */
+    `<div class="mb-3">
+        <label class="form-label">Nombre</label>
+        <input
+          type="text"
+          class="form-control"
+          id="nombreUpdate"
+          value="${productos[index].nombre}"
+        />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Detalle</label>
+        <textarea class="form-control" id="detalleUpdate" rows="3">
+${productos[index].detalle}</textarea
+        >
+      </div>
+      <div class="d-flex justify-content-between mb-3">
+        <div class="col me-2">
+          <label class="form-label">URL de imagen</label>
+          <input
+            type="text"
+            class="form-control"
+            id="imagenUpdate"
+            value="${productos[index].imagen}"
+          />
+        </div>
+        <div class="col">
+          <label class="form-label">Precio</label>
+          <input
+            type="number"
+            class="form-control"
+            id="precioUpdate"
+            value="${productos[index].precio}"
+          />
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-end">
+        <button
+          class="btn btn-warning"
+          type="button"
+          onclick="actualizarProducto(${index})"
+        >
+          Actualizar
+        </button>
+      </div> `;
+  formularioUpDate.innerHTML = contenidoFormulario;
+  document.querySelector(".modal-body").append(formularioUpDate);
+
+  myModal.show();
+};
+
 const crearProductos = (e) => {
   e.preventDefault();
   //crear id del producto
@@ -48,6 +109,17 @@ const borrarProducto = (index) => {
   }
 };
 
+const actualizarProducto = (index) => {
+  productos[index].nombre = querySelector("#nombreUpdate").value;
+  productos[index].detalle = querySelector("#detalleUpdate").value;
+  productos[index].imagen = querySelector("#imagenUpdate").value;
+  productos[index].precio = querySelector("#precioUpdate").value;
+
+  localStorage.setItem("productos", JSON.stringify(productos));
+  cargarTabla();
+  myModal.hide();
+};
+
 const cargarTabla = () => {
   cuerpoTabla.innerHTML = " ";
   //por cada elemento del arreglo producto crear una fila con las celdas.
@@ -71,7 +143,7 @@ const cargarTabla = () => {
           <button class="btn btn-danger" onclick="borrarProducto(${index})">
             X
           </button>
-          <button class="btn btn-warning">Edit</button>
+          <button class="btn btn-warning" onclick="abrirModal(${index})">&#9998</button>
         </td>
       `;
 
